f4908565bcb33661aa6ffe18c6fd9a2e86664eac
Dawsey <43346309+Dawsey@users.noreply.github.com>
2018-12-06 00:25:14 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,62 +2,39 @@
 
 library(readr)
 # Read in the data
-    xTest <- read_table2("X_test.txt", col_names = FALSE)
-    xTrain <- read_table2("X_train.txt", col_names = FALSE)
-    features <- read_table2("features.txt", col_names = FALSE)
-    
-
-    # Review the data
-    nrow(xTrain)+nrow(xTest) # Total length of both datasets - should = 10299
-    head(xTest)
-    head(xTrain)
-
-    summary(complete.cases(xTest ))
-    summary(complete.cases(xTrain ))
-    sum(complete.cases(xTrain))-nrow(xTrain)
-    sum(complete.cases(xTest))-nrow(xTest)
-
+    features <- read_table2("features.txt", col_names = c("n","feature")) #col.names = 
+    xTest <- read_table2("X_test.txt", col_names = features$feature)
+    xTrain <- read_table2("X_train.txt", col_names = features$feature)
+    yTest <- read_table2("y_test.txt", col_names = "activity_id")
+    yTrain <- read_table2("y_train.txt", col_names = "activity_id")
+    subTrain <- read_table2("subject_train.txt", col_names = "subject")
+    subTest <- read_table2("subject_test.txt", col_names =  "subject")
+    activityLabels <- read_table2("activity_labels.txt", col_names = c("activity_id","activity"))
 
 # Merge the datasets
-    merged <- rbind(xTest, xTrain)
-    names(merged) <- features$X2 # need to clean up features names
-
-# Tidy the data
-    # Remove parenthesis from column names using gsub
-    names(merged) <- (gsub("[()]","",names(merged)))
-
-    # Check for completeness
-    summary(complete.cases(merged)) # All True (no missing cases)
+    xAll <- rbind(xTest, xTrain) # Combine the measurements
+    #xAll <- xAll[,grep("mean|std", tolower(names(xAll)))] # tolower used as some means have capital M.
+    xAll <- xAll[,grep("mean|std", names(xAll))] # remove lower case means and std.
     
-# RUle of Tidy data - Names of Variables should not be duplicated
-    # The following if statement checks for duplicate column names and then
-    # presents the user with a selection to show the names or not.
+    yAll <- rbind(yTest, yTrain) # Combine the activities
+ 
+    subjectAll <- rbind(subTest, subTrain) # Combine the subject IDs
     
-    if (any(duplicated(names(merged))==TRUE)) { # Duplicates present
-           
-        if(menu(c("Yes", "No"), 
-                graphics=FALSE, 
-                title=paste0("There are ",
-                             length(which(duplicated(names(merged)))),
-                             " duplicate column names, would you like to see them?"))==1) {
-            
-            # 'Yes' is selected so list the duplicate column names   
-            names(merged[(which(duplicated(names(merged))))])
+    combined.df <- cbind(subjectAll,yAll,xAll)
+    combined.df <- merge(combined.df, activityLabels, 
+                         by.x = "activity_id", by.y = "activity_id")
 
-        } # 'No' Selected so do nothing.
+# Group and calculate mean values using dplyr
+    library(dplyr)
+    
+        finalData <- combined.df %>%
+            group_by(subject, activity_id, activity) %>%
+                summarise_all(funs(mean)) 
         
-    } # No duplicates, so do nothing.
-    
-    # Duplicate examples:
-    names(merged[,c(331,317)])
-    # Do they contain duplicate data?
-    summary(merged[,c(331,317)])
-    # Apparently not.
-    
-# Extract only the mean and stdev measurements
-    
-    # mean(): Mean value
-    # std(): Standard deviation
-    
-    mean <- merged[,grep("mean|std", tolower(names(merged)))] # tolower used as some means have capital M.
-    
+        finalData <- finalData[,-2] # Remove the Activity ID leaving only the description
+        names(finalData) <- (gsub("[()]","",names(finalData))) # Remove parenthesis
+
+# Final Checks
+        # Check for completeness
+        summary(complete.cases(finalData)) # 180 True (i.e no missing cases)
+        
