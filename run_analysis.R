8f6a265e6252eb335562ca9d52d7d33c88a33d5a
Dawsey <43346309+Dawsey@users.noreply.github.com>
2018-12-16 22:14:00 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,20 +1,28 @@
 # Week 4 Assignment run_analysis
 
 library(readr)
-# Read in the data
-    features <- read_table2("features.txt", col_names = c("n","feature")) #col.names = 
-    xTest <- read_table2("X_test.txt", col_names = features$feature)
-    xTrain <- read_table2("X_train.txt", col_names = features$feature)
-    yTest <- read_table2("y_test.txt", col_names = "activity_id")
-    yTrain <- read_table2("y_train.txt", col_names = "activity_id")
-    subTrain <- read_table2("subject_train.txt", col_names = "subject")
-    subTest <- read_table2("subject_test.txt", col_names =  "subject")
-    activityLabels <- read_table2("activity_labels.txt", col_names = c("activity_id","activity"))
+# Read in the data and set column names on import
+    features <- read_table2("./UCI HAR Dataset/features.txt", 
+                            col_names = c("n","feature"))
+    xTest <- read_table2("./UCI HAR Dataset/test/X_test.txt", 
+                            col_names = features$feature)
+    xTrain <- read_table2("./UCI HAR Dataset/train/X_train.txt", 
+                            col_names = features$feature)
+    yTest <- read_table2("./UCI HAR Dataset/test/y_test.txt", 
+                            col_names = "activity_id")
+    yTrain <- read_table2("./UCI HAR Dataset/train/y_train.txt", 
+                            col_names = "activity_id")
+    subTrain <- read_table2("./UCI HAR Dataset/train/subject_train.txt", 
+                            col_names = "subject")
+    subTest <- read_table2("./UCI HAR Dataset/test/subject_test.txt", 
+                            col_names =  "subject")
+    activityLabels <- read_table2("./UCI HAR Dataset/activity_labels.txt", 
+                            col_names = c("activity_id","activity"))
 
 # Merge the datasets
     xAll <- rbind(xTest, xTrain) # Combine the measurements
-    #xAll <- xAll[,grep("mean|std", tolower(names(xAll)))] # tolower used as some means have capital M.
-    xAll <- xAll[,grep("mean|std", names(xAll))] # remove lower case means and std.
+    xAll <- xAll[,grep("mean|std", tolower(names(xAll)))] # tolower used as some means have capital M.
+    #xAll <- xAll[,grep("mean|std", names(xAll))] # remove only the lower case means and std.
     
     yAll <- rbind(yTest, yTrain) # Combine the activities
  
@@ -24,17 +32,27 @@ library(readr)
     combined.df <- merge(combined.df, activityLabels, 
                          by.x = "activity_id", by.y = "activity_id")
 
-# Group and calculate mean values using dplyr
+# Group by Subject and calculate mean values using dplyr
     library(dplyr)
     
-        finalData <- combined.df %>%
-            group_by(subject, activity_id, activity) %>%
-                summarise_all(funs(mean)) 
+    finalData <- combined.df %>%
+                    group_by(subject, activity_id, activity) %>%
+                    summarise_all(funs(mean)) 
         
+    # Tidy the data
         finalData <- finalData[,-2] # Remove the Activity ID leaving only the description
-        names(finalData) <- (gsub("[()]","",names(finalData))) # Remove parenthesis
-
+        names(finalData) <- (gsub("[()]","", names(finalData))) # Remove parenthesis
+        names(finalData) <- (gsub("X","Xaxis",names(finalData)))
+        names(finalData) <- (gsub("Y","Yaxis",names(finalData)))
+        names(finalData) <- (gsub("Z","Zaxis",names(finalData)))
+        names(finalData) <- (gsub("tB","time_B",names(finalData)))
+        names(finalData) <- (gsub("tG","time_G",names(finalData)))
+        names(finalData) <- (gsub("fG","freq_G",names(finalData)))
+        names(finalData) <- (gsub("fB","freq_B",names(finalData)))
+        names(finalData) <- (gsub("-","_",names(finalData)))
+        
 # Final Checks
         # Check for completeness
-        summary(complete.cases(finalData)) # 180 True (i.e no missing cases)
+        summary(complete.cases(finalData)) 
+        # 180 True (i.e no missing cases)
         
